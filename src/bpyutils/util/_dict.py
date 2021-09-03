c93ee788a9ed01be65a191c9c1636aab6ec431ed
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-09-03 11:32:59 -0500

    chore: Added docstrings for merge_dict

--- src/bpyutils/util/_dict.py
+++ src/bpyutils/util/_dict.py
@@ -2,6 +2,18 @@
 import collections
 
 def merge_dict(*args):
+    """
+        Merge Dictionaries.
+        
+        :param args: arguments of dictionaries to be merged. `merge_dict` will override keys from right to left.
+
+        Example
+            >>> from bpyutils.util._dict import merge_dict
+            >>> merge_dict({ 'foo': 'bar' }, { 'bar': 'baz' }, { 'baz': 'boo' })
+            {'foo': 'bar', 'bar': 'baz', 'baz': 'boo'}
+            >>> merge_dict({ 'foo': 'bar' }, { 'foo': 'baz', 'bar': 'boo' })
+            {'foo': 'baz', 'bar': 'boo'}
+    """
     merged = dict()
 
     for arg in args:
