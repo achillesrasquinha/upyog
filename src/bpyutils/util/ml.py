05ec5d98c5218745fde8ad59e5785c07842c77f9
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-10-30 16:15:06 -0500

    feat: add get_data_dir in ml utils

---
+++ src/bpyutils/util/ml.py
@@ -0,0 +1,17 @@
+import os.path as osp
+
+from bpyutils.util.system  import makedirs
+from bpyutils.util.environ import getenv
+from bpyutils.config import get_config_path
+
+def get_data_dir(name, data_dir = None):
+    prefix = name.upper()
+    config_path = get_config_path(name)
+
+    data_dir = data_dir \
+        or getenv("DATA_DIR", prefix = prefix) \
+        or osp.join(config_path, "data")
+
+    makedirs(data_dir, exist_ok = True)
+
+    return data_dir
