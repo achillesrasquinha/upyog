a7d2af8d9ee30ac566e8864e5b481c37916bdcb7
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-12-09 11:05:42 -0600

    fix: add args to csv read

--- src/bpyutils/util/_csv.py
+++ src/bpyutils/util/_csv.py
@@ -3,11 +3,11 @@ import csv
 from bpyutils.util._dict import dict_from_list
 from bpyutils.util.types import lmap, auto_typecast
 
-def read(path):
+def read(path, *args, **kwargs):
     data = []
     
     with open(path) as f:
-        reader = csv.reader(f)
+        reader = csv.reader(f, *args, **kwargs)
         header = next(reader, None)
 
         data = lmap(lambda x: dict_from_list(header, lmap(auto_typecast, x)), reader)
