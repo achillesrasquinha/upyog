7a429b1bb9b3abdfdf56c609023513aca08288e5
Achilles Gasper Rasquinha <arasquin@amazon.com>
2023-03-14 19:47:42 +0000

    feat: add retry sleep

--- src/upyog/util/misc.py
+++ src/upyog/util/misc.py
@@ -1,8 +1,9 @@
 import upyog as upy
+import time
 
 LOG = upy.get_logger(__name__)
 
-def retry(n):
+def retry(n, every = 1):
     """
     Decorator to retry a function n times.
     """
@@ -12,6 +13,7 @@ def retry(n):
                 try:
                     return func(*args, **kwargs)
                 except Exception as e:
+                    time.sleep(every)
                     LOG.warning("Error: %s occured. Retrying %s/%s", e, i + 1, n)
                     if i == n - 1:
                         raise e
