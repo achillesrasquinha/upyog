06e5248028bce3e8c32114b0d7a7a974e56d2525
Achilles Gasper Rasquinha <arasquin@amazon.com>
2023-08-01 01:03:39 +0000

    fix: log

--- src/upyog/log.py
+++ src/upyog/log.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 # imports - standard imports
 import logging
-import time
+import time, copy
 
 # imports - module imports
 from upyog.util.cli import CYAN, GRAY, ORANGE
