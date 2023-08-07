c1d6e741466fbf200a89281416d9f73225c93fd4
Achilles Gasper Rasquinha <arasquin@amazon.com>
2023-08-07 15:34:55 +0000

    fix: add callable to clause

--- src/upyog/util/query.py
+++ src/upyog/util/query.py
@@ -13,31 +13,38 @@ def where(data, clause, other = False):
     for record in data:
         add = True
 
-        for key, value in upy.iteritems(clause):
-            if key in record:
-                if isinstance(value, upy.Mapping):
-                    for op, val in upy.iteritems(value):
-                        constraint = upy.Op[op]
+        if callable(clause):
+            if not clause(record):
+                add = False
+                break
+        elif isinstance(clause, upy._compat.Mapping):
+            for key, value in upy.iteritems(clause):
+                if key in record:
+                    if isinstance(value, upy.Mapping):
+                        for op, val in upy.iteritems(value):
+                            constraint = upy.Op[op]
 
-                        op1 = record[key]
-                        op2 = val
-                        
-                        try:
-                            if not constraint(op1, op2):
+                            op1 = record[key]
+                            op2 = val
+                            
+                            try:
+                                if not constraint(op1, op2):
+                                    add = False
+                                    break
+                            except TypeError:
                                 add = False
                                 break
-                        except TypeError:
+                    elif callable(value):
+                        if not value(record[key]):
                             add = False
                             break
-                elif callable(value):
-                    if not value(record[key]):
-                        add = False
-                        break
-                else:
-                    # TODO: callable for value?
-                    if record[key] != value:
-                        add = False
-                        break
+                    else:
+                        # TODO: callable for value?
+                        if record[key] != value:
+                            add = False
+                            break
+        else:
+            raise TypeError(f"Invalid clause type: {type(clause)}")
 
         if add:
             results.append(record)
